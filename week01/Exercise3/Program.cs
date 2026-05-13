dc64ecf6cf002739f6e8aa7b0ffd69f0001bbeb9
Alfredo Cassanje <alfredocassanj@gmail.com>
2026-05-13 06:50:12 +0100

    G

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        Console.WriteLine("Welcome to Guess My Number!");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+                Console.WriteLine("Higher");
+            else if (magicNumber < guess)
+                Console.WriteLine("Lower");
+            else
+                Console.WriteLine("You guessed it!");
+        }
     }
 }
