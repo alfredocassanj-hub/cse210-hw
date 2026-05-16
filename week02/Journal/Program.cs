3aa764c77e822f525f9271fcf890f928c860d6f5
Alfredo Cassanje <alfredocassanj@gmail.com>
2026-05-17 00:21:06 +0100

    r

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
-class Program
+public class Journal
 {
-    static void Main(string[] args)
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        _entries.Add(entry);
+    }
+
+    public void DisplayAll()
+    {
+        if (_entries.Count == 0)
+        {
+            Console.WriteLine("No entries found.");
+            return;
+        }
+
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter output = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                output.WriteLine(entry.GetFormattedEntry());
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            Entry entry = new Entry();
+            entry.LoadFromString(line);
+            _entries.Add(entry);
+        }
     }
 }
